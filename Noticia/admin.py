c2802dd58469651dcf2e512935190ebd33a8edab
Igor <idqigordantas@gmail.com>
2021-11-15 22:59:41 -0300

    alterações de linguagem e algumas alterações de exibição no /admin

--- Noticia/admin.py
+++ Noticia/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Noticia.models import noticias
 
-# Register your models here.
+class Noticia(admin.ModelAdmin):
+    list_display = ('id_not', 'titulo', 'conteudo', 'autor', 'publicado', 'data_criacao')
+    list_display_links = ('id_not',)
+    search_fields = ('id_not', 'titulo',)
+admin.site.register(noticias, Noticia)
