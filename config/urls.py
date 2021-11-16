65c682f252d26e5cbfadd0d49604e3305c405e0a
Igor <idqigordantas@gmail.com>
2021-11-15 22:26:01 -0300

    configurando router

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from Noticia.api import viewsets as notViewSets
+
+router = routers.DefaultRouter()
+router.register(r'Noticia', notViewSets.noticiasViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
